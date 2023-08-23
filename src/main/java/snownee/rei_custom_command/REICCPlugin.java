73d85dbf0799d6a561a9c85cb7651a4d64be244b
Snownee <i@snownee.com>
2023-08-23 19:19:36 +0800

    fix: json error handling

--- src/main/java/snownee/rei_custom_command/REICCPlugin.java
+++ src/main/java/snownee/rei_custom_command/REICCPlugin.java
@@ -8,6 +8,8 @@ import me.shedaniel.rei.api.client.favorites.FavoriteEntryType.Registry;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.forge.REIPluginClient;
 import me.shedaniel.rei.impl.client.gui.widget.search.OverlaySearchField;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.toasts.SystemToast;
 import net.minecraft.network.chat.Component;
 
 @REIPluginClient
@@ -15,12 +17,6 @@ public class REICCPlugin implements REIClientPlugin {
 
 	private static final Pattern PATTERN = Pattern.compile("^([^/\\\"][^/]*|\\\".+\\\")?/(.+)$", Pattern.MULTILINE);
 	private static final Pattern FORMATTING_PATTERN = Pattern.compile("(?i)\\$([0-9A-FK-OR])");
-
-	@Override
-	public void registerFavorites(Registry registry) {
-		registry.register(CustomCommandFavoriteEntry.ID, CustomCommandFavoriteEntry.Type.INSTANCE);
-		registry.getOrCrateSection(Component.translatable(CustomCommandFavoriteEntry.TRANSLATION_KEY)).add(CustomCommandFavoriteEntry.DEFAULT);
-	}
 
 	public static boolean onPressEnterInSearch(OverlaySearchField searchField, int keyCode) {
 		if (keyCode != 257 && keyCode != 335) {
@@ -35,11 +31,21 @@ public class REICCPlugin implements REIClientPlugin {
 		}
 		String titleStr = matcher.group(1);
 		String command = matcher.group(2);
-		Component title = null;
-		if (titleStr != null && titleStr.startsWith("{")) {
-			title = Component.Serializer.fromJson(titleStr);
-		} else if (titleStr != null) {
-			title = Component.literal(FORMATTING_PATTERN.matcher(titleStr).replaceAll("§$1"));
+		Component title;
+		if (titleStr != null) {
+			if (titleStr.startsWith("{")) {
+				try {
+					title = Component.Serializer.fromJson(titleStr);
+				} catch (Exception e) {
+					Minecraft mc = Minecraft.getInstance();
+					SystemToast.SystemToastIds ids = SystemToast.SystemToastIds.UNSECURE_SERVER_WARNING; // make the toast persists longer
+					SystemToast toast = SystemToast.multiline(mc, ids, Component.translatable("rei-custom-command.sth-wrong"), Component.literal(e.getLocalizedMessage()));
+					mc.getToasts().addToast(toast);
+					return false;
+				}
+			} else {
+				title = Component.literal(FORMATTING_PATTERN.matcher(titleStr).replaceAll("ยง$1"));
+			}
 		} else {
 			title = Component.empty();
 		}
@@ -47,6 +53,12 @@ public class REICCPlugin implements REIClientPlugin {
 		ConfigObject.getInstance().getFavoriteEntries().add(entry);
 		searchField.setText("");
 		return true;
+	}
+
+	@Override
+	public void registerFavorites(Registry registry) {
+		registry.register(CustomCommandFavoriteEntry.ID, CustomCommandFavoriteEntry.Type.INSTANCE);
+		registry.getOrCrateSection(Component.translatable(CustomCommandFavoriteEntry.TRANSLATION_KEY)).add(CustomCommandFavoriteEntry.DEFAULT);
 	}
 
 }
